e559b6b8d37b2204e13d5dd93631461ee4d95695
meichthys <github.com@meichthys.com>
2026-03-23 06:38:00 +0000

    fix guest access to published audio recordings

--- church/church_study/doctype/sermon/sermon.py
+++ church/church_study/doctype/sermon/sermon.py
@@ -11,3 +11,23 @@ class Sermon(Document):
 		if not self.is_new() and self.has_value_changed("title") and self.title != self.name:
 			rename_doc(self.doctype, self.name, self.title, merge=False)
 			self.name = self.title
+
+	def on_update(self):
+		if self.audio_recording:
+			file_doc = frappe.db.get_value(
+				"File",
+				{
+					"file_url": self.audio_recording,
+					"attached_to_doctype": "Sermon",
+					"attached_to_name": self.name,
+				},
+				"name",
+			)
+			if file_doc:
+				f = frappe.get_doc("File", file_doc)
+				should_be_private = not self.publish
+				if f.is_private != should_be_private:
+					f.is_private = should_be_private
+					f.save(ignore_permissions=True)
+					if f.file_url != self.audio_recording:
+						frappe.db.set_value("Sermon", self.name, "audio_recording", f.file_url)
