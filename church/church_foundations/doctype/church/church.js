38041d382669d25ab22f477dce3e636bdad56196
meichthys <github.com@meichthys.com>
2026-03-27 05:05:29 +0000

    make church document publishable or not

--- church/church_foundations/doctype/church/church.js
+++ church/church_foundations/doctype/church/church.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2026, meichthys and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Church", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Church", {
+	refresh(frm) {
+		if (frm.doc.publish) {
+			frm.set_intro('🌐 This church is published to the public website', 'blue');
+		}
+	},
+});
