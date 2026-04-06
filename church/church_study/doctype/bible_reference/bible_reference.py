ae8776d990cbcdeda48bc64833676e1de1c51d0d
meichthys <github.com@meichthys.com>
2026-04-06 03:41:19 +0000

    Fix beliefs web page and verse/reference title fields

--- church/church_study/doctype/bible_reference/bible_reference.py
+++ church/church_study/doctype/bible_reference/bible_reference.py
@@ -6,17 +6,13 @@ from frappe.model.document import Document
 
 
 class BibleReference(Document):
-	pass
-
 	def validate(self):
 		self.reference = self.compute_reference()
 
 	def compute_reference(self):
-		# Determine the translation text to display based on the abbreviation of the Bible Translation
+		start = frappe.db.get_value("Bible Verse", self.start_verse, "reference") if self.start_verse else ""
+		end = frappe.db.get_value("Bible Verse", self.end_verse, "reference") if self.end_verse else ""
 		abbr = frappe.db.get_value("Bible Translation", self.translation, "abbreviation")
 		translation_text = f" ({abbr})" if abbr else ""
-		end_verse_text = (
-			f" - {self.end_verse}" if self.end_verse and self.end_verse != self.start_verse else ""
-		)
-
-		return f"{self.start_verse}{end_verse_text}{translation_text}"
+		end_verse_text = f" - {end}" if end and self.end_verse != self.start_verse else ""
+		return f"{start}{end_verse_text}{translation_text}"
--- church/church_study/doctype/bible_verse/bible_verse.py
+++ church/church_study/doctype/bible_verse/bible_verse.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2025, meichthys and contributors
 # For license information, please see license.txt
 
+import frappe
 from frappe.model.document import Document
 
 
 class BibleVerse(Document):
 	def before_save(self):
-		self.title = f"{self.book} {self.chapter}:{self.verse}"
+		book_name = frappe.db.get_value("Bible Book", self.book, "book") if self.book else ""
+		self.reference = f"{book_name} {self.chapter}:{self.verse}"
