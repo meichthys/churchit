4abdff29c985217bc26b91ab3463dc28795d7fb9
meichthys <github.com@meichthys.com>
2026-04-16 02:27:17 +0000

    calculate age and anniversary years

--- church/church_people/doctype/person/person.js
+++ church/church_people/doctype/person/person.js
@@ -3,6 +3,41 @@
 
 frappe.ui.form.on("Person", {
 	refresh(frm) {
+		// Calculate age from birthday
+		if (frm.doc.birthday) {
+			const today = frappe.datetime.get_today();
+			const birthDate = new Date(frm.doc.birthday);
+			const todayDate = new Date(today);
+
+			let age = todayDate.getFullYear() - birthDate.getFullYear();
+
+			if (todayDate.getMonth() < birthDate.getMonth() ||
+			    (todayDate.getMonth() === birthDate.getMonth() && todayDate.getDate() < birthDate.getDate())) {
+				age--;
+			}
+
+			frm.doc.age = age;
+			frm.refresh_field('age');
+		}
+
+		// Calculate marriage years from anniversary
+		if (frm.doc.anniversary) {
+			const today = frappe.datetime.get_today();
+			const anniversary = frm.doc.anniversary;
+			const anniversaryDate = new Date(anniversary);
+			const todayDate = new Date(today);
+
+			let years = todayDate.getFullYear() - anniversaryDate.getFullYear();
+
+			if (todayDate.getMonth() < anniversaryDate.getMonth() ||
+			    (todayDate.getMonth() === anniversaryDate.getMonth() && todayDate.getDate() < anniversaryDate.getDate())) {
+				years--;
+			}
+
+			frm.doc.marriage_years = years;
+			frm.refresh_field('marriage_years');
+		}
+
 		// Add 'New Family From Person' button if Last Name is populated and person is not already in a family
 		if (frm.doc.last_name && !frm.doc.family) {
 			frm.add_custom_button(__('New Family From Person'), function () {
