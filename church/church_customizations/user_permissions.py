bf29f5450abe8e601c03ddb0f35ae9a2c81bc1f4
meichthys <github.com@meichthys.com>
2026-05-21 03:59:18 +0000

    remove unused code

--- church/church_customizations/user_permissions.py
+++
@@ -1,2 +0,0 @@
-# Copyright (c) 2025, meichthys and contributors
-# For license information, please see license.txt
--- church/hooks.py
+++ church/hooks.py
@@ -68,7 +68,6 @@ web_include_js = ["/assets/church/js/portal_groups.js"]
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"User": "public/js/user.js"}
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
